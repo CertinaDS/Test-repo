349446736e5965b47ae0ef2ed31674d7332f4ba6
Certina DS <imozhaev@yandex.ru>
2024-07-20 12:55:46 +0300

    Косметические правки

--- Modul_4/Find_in_sortlist.py
+++ Modul_4/Find_in_sortlist.py
@@ -16,10 +16,5 @@ num_list = random.sample(range(1, 1500), 50)
 sorted_num_list = sorted(num_list)
 number = random.choice(sorted_num_list)
 print(f"Список элементов:\n{sorted_num_list}\n")
-<<<<<<< HEAD
 index = findIndex(sorted_num_list, number)
 print(f"Случайный элемент {number} находится по индексу {index}")
-=======
-answer = findIndex(sorted_num_list, number)
-print(f"Случайный элемент {number} находится по индексу {answer}")
->>>>>>> cff5fc21fd29053da555795391e25ea843ad589c
