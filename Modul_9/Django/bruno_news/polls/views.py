8b32746ac9a344885eb56d65998d59e49bb7aa32
Certina DS <imozhaev@yandex.ru>
2025-05-22 12:58:47 +0300

    Добавлен Модуль 9

---
+++ Modul_9/Django/bruno_news/polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Ну здравствуй, мир. Вы находитесь в индексе опросов общественного мнения.")
+# Create your views here.
