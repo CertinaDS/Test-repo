7a3d3a567210066eb5c55fe620bea03d17d0ec4e
Certina DS <imozhaev@yandex.ru>
2025-05-27 14:44:12 +0300

    Saved progress..

--- Modul_9/Django/bruno_news/polls/urls.py
+++ Modul_9/Django/bruno_news/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from polls.views import index
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('polls/', index),
 ]
--- Modul_9/Django/bruno_news/polls/views.py
+++ Modul_9/Django/bruno_news/polls/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Ну здравствуй, мир. Вы находитесь в индексе опросов общественного мнения.")
 # Create your views here.
